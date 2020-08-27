db4be8252ed46bfbedc3b9f3c8b5a55bcc6338e4
harmony.is.valued@gmail.com <53105202+zetzetn@users.noreply.github.com>
2020-08-27 21:24:56 +0900

    sample03 現在の秒 を表示する

---
+++ sample03.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<!-- ここにプログラムを記述します -->
+<?php
+
+print(date('s'));
+
+?>
+
+</pre>
+</main>
+</body>    
+</html>
